9d7857fd3956f14af5c7a2a63a1b3f6eea4820d1
imyofather79 <chunsum708@gmail.com>
2021-07-27 00:30:07 -0600

    commit tree 3ac3d2ae1c5af8126e5b377fe4de30f64b725c6d

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const eachlink = links.map((link) => (
+      <a key={link} href={ "#"+link }>{link}</a>
+  ));
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{eachlink}</nav> 
+
 }
 
 export default NavBar;
